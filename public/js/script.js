431531d9d3cea46705d05eed8a37481a73891caf
Ahmed <mahmedk91@gmail.com>
2016-01-12 22:48:08 +0100

    dfd

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,12 @@
 var socket = io();
 socket.on('schedule', function(schedule){
     schedule.forEach(function(item, index){
-      $( "#contents" ).append( "'<tr><td>" + item.uid + "</td>
-                  <td>" + item.uid + "</td></tr>'" );
+      $( "#contents" ).append( "'<tr><td>" + item.uid + "</td></tr>'");
     });
 });
 
 socket.on('newData', function(newData){
+    alert(newData);
+    console.log(newData);
     $( "#contents" ).append( "'<tr><td>" + newData.uid + "</td></tr>'" );
 });
